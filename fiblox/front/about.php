189fb96fc074d15dcb046d5bb5a23ee3a7b96def
senerh <hakan.sener@iut-valence.fr>
2014-06-13 09:42:11 +0200

    Update about.php

--- fiblox/front/about.php
+++ fiblox/front/about.php
@@ -3,7 +3,9 @@ include (__DIR__."/../../../inc/includes.php");
 
 Html::header('FIBlox',$_SERVER['PHP_SELF'],"plugins","fiblox","about");
 
-echo 'À propos du plugin<br />';
+echo '<h2>À propos</h2>';
+echo '<strong>FIBlox</strong> a été développé dans le cadre dun stage de DUT Informatique au sein de la DSI de l\'UPMF.<br />';
+echo 'La documentation ainsi que le plugin sont disponibles <a href="https://github.com/senerh/FIBlox">à cette adresse.</a><br />';
 
 Html::footer();
 ?>
